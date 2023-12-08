b0384a903cdfcfd5b9596507e8f26d4a49e6b931
DaBaivvi <99689877+DaBaivvi@users.noreply.github.com>
2023-12-08 13:29:21 +0100

    Videopage: 1. check detail in admin.py 2. create forms.py and set videoform to models.py 3. define videoforms such as titel length and video_file.. in models.py 4. define upload_video and video_list request in views.py 5. create two htmls in template (upload_video and video_list) 6. set videos and upload in urls.py
    FrameMarker:
    In settings.py
    1. set 'homepage and videopage'in INSTALLED APP
    2. set 'django.template.context_processors.media'in templates
    3. set media root
    
    In ulrs.py
    1. add static file by class urlpatterns

--- FrameMarker/videopage/url.py
+++ FrameMarker/videopage/url.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='videos'),
+    path('upload/', views.upload_video, name='upload_video'),
+    path('videos/', views.video_list, name='video_list'),
 ]
--- FrameMarker/videopage/views.py
+++ FrameMarker/videopage/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import VideoForm
+from .models import Video
 
-# Create your views here.
+def upload_video(request):
+       if request.method == 'POST':
+            form = VideoForm(request.POST, request.FILES)
+            if form.is_valid():
+               form.save()
+               return redirect('video_list')  # Redirect to video list page
+       else:
+           form = VideoForm()
+           return render(request, 'upload_video.html', {'form': form})
+ 
+def video_list(request):
+     videos = Video.objects.all()
+     return render(request, 'video_list.html', {'videos': videos})
+
 def index(request):
     return render(request, 'Videos.html')
