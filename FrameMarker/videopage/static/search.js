e46f1deaba340c5007eb82bbeeafad6ef18a72cd
ZhiqianYu <103505612+ZhiqianYu@users.noreply.github.com>
2023-12-08 14:00:18 +0100

    resolve css js problem

--- FrameMarker/videopage/static/search.js
+++
@@ -1,6 +0,0 @@
-function performSearch() {
-    const searchQuery = document.querySelector('.search-container input[type=text]').value;
-    if (searchQuery.trim() !== '') {
-        window.location.href = `videos.html?search=${encodeURIComponent(searchQuery)}`;
-    }
-}
