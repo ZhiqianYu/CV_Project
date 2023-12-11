171a3313b001d851144156b91fb666b845f5ea1d
ZhiqianYu <103505612+ZhiqianYu@users.noreply.github.com>
2023-12-11 02:38:57 +0100

    上传可以成功，但是无法弹出选项提示

---
+++ FrameMarker/homepage/static/upload.js
@@ -0,0 +1,37 @@
+function showFileName(input) {
+    const file = input.files[0];
+    const fileNameArea = document.getElementById('fileNameArea');
+    const chooseFileBtn = document.getElementById('chooseFileBtn');
+    const uploadButton = document.querySelector('button[type="button"]');
+
+    if (file) {
+        fileNameArea.textContent = 'Selected File: ' + file.name;
+        fileNameArea.style.display = 'block';
+        chooseFileBtn.textContent = 'Change File';
+        uploadButton.style.display = 'inline-block';
+    }
+}
+
+function uploadFile() {
+    document.getElementById('uploadForm').submit();
+}
+
+function handleUploadResponse(response) {
+    if (response.status === 'Upload Success') {
+        var uploadAnother = confirm('视频成功上传，是否上传另一个视频？');
+        if (uploadAnother) {
+            // 清空文件输入框
+            document.getElementById('fileInput').value = '';
+            // 清空文件名显示区域
+            document.getElementById('fileNameArea').textContent = '';
+            document.getElementById('fileNameArea').style.display = 'none';
+            // 恢复按钮状态
+            document.getElementById('chooseFileBtn').textContent = 'Choose File';
+            document.querySelector('button[type="button"]').style.display = 'none';
+        } else {
+            window.location.href = '/videopage/';
+        }
+    } else if (response.status === 'Upload Failed') {
+        alert('文件上传失败：' + response.message);
+    }
+}
