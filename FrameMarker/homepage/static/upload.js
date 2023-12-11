ed1f6c2ac894873348db0ba1a514fa051aaf0e19
ZhiqianYu <103505612+ZhiqianYu@users.noreply.github.com>
2023-12-11 03:21:38 +0100

    修正问题 上传弹出提示

--- FrameMarker/homepage/static/upload.js
+++ FrameMarker/homepage/static/upload.js
@@ -13,7 +13,18 @@ function showFileName(input) {
 }
 
 function uploadFile() {
-    document.getElementById('uploadForm').submit();
+    var form = document.getElementById('uploadForm');
+    var formData = new FormData(form);
+
+    fetch(form.action, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => handleUploadResponse(data))
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
 
 function handleUploadResponse(response) {
