a1898116d232f5be632588c8ed525aa5a76bf6e8
ZhiqianYu <103505612+ZhiqianYu@users.noreply.github.com>
2024-01-20 16:21:41 +0100

    list displyed in admin

--- FrameMarker/annotation/admin.py
+++ FrameMarker/annotation/admin.py
@@ -4,7 +4,7 @@ from .models import VideoFrames, FrameAnnotations
 class VideoFramesAdmin(admin.ModelAdmin):
     list_display = ('video_name', 'total_frames_60', 'total_frames_4', 'video_frames_total')
     search_fields = ('video__file_name', 'has_frames_60', 'total_frames_60', 'has_frames_4', 'total_frames_4', 'video_frames_total', 'frame_folder_path', 'frame_folder_path_4', 'frame_folder_path_60')
-    list_filter = ('video__file_name', 'has_frames_60', 'total_frames_60', 'has_frames_4', 'total_frames_4', 'video_frames_total', 'frame_folder_path', 'frame_folder_path_4', 'frame_folder_path_60')
+    fields = ('video', 'has_frames_60', 'total_frames_60', 'has_frames_4', 'total_frames_4', 'video_frames_total', 'frame_folder_path', 'frame_folder_path_4', 'frame_folder_path_60')
 
     def video_name(self, obj):
         return obj.video.file_name
