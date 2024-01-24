a32e0ce05ee776a682db1afbdc881521a130120f
ZhiqianYu <103505612+ZhiqianYu@users.noreply.github.com>
2024-01-24 13:50:17 +0100

    export page desgin

--- FrameMarker/exportpage/models.py
+++ FrameMarker/exportpage/models.py
@@ -1,2 +1,11 @@
 from django.db import models
+from annotation.models import FrameAnnotations
 
+class AnnotationExport(models.Model):
+    choosed_frames = models.ManyToManyField(FrameAnnotations)
+    export_name = models.CharField(max_length=255, default="")
+    export_time = models.DateTimeField(auto_now_add=True)
+    export_user = models.CharField(max_length=255, default="")
+    export_path = models.CharField(max_length=255, default="")
+    export_file_type = models.CharField(max_length=255, default="")
+    
--- FrameMarker/exportpage/views.py
+++ FrameMarker/exportpage/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from annotation.models import FrameAnnotations
+from homepage.models import Video
 
-# Create your views here.
+def exportpage(request):
+    return render(request, 'exportpage.html')
+
+def exportfromannotation(request, video_id):
+    video = get_object_or_404(Video, pk=video_id)
+        # 在这里处理视频信息，以及获取相关的帧标注数据
+    frame_annotations = FrameAnnotations.objects.filter(video=video)
+
+
+    # 在这里可以进行其他的处理，例如生成 CSV 数据等
+
+    # 渲染 exportpage 页面
+    return render(request, 'exportpage.html', {'video': video, 'frame_annotations': frame_annotations})
